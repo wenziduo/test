d28c1d002a3c522ae22cef73d68f762f8db91b8f
caiwenduo <caiwenduo@163.com>
2019-11-14 11:32:48 +0800

    add

--- react-next/pages/task.js
+++ react-next/pages/task.js
@@ -1,14 +1,23 @@
-import { withRouter } from 'next/router'
-const Task = ({ router, fzr }) => (
-  <span>
-    {router.query.taskName} 任务负责人：{fzr}
-  </span>
-)
-Task.getInitialProps = async () => {
-  console.log('----------服务器端渲染-------------')
-  await new Promise(resolve => {
-    setTimeout(resolve, 5000)
-  })
-  return { fzr: 'chatwei' }
+import { withRouter } from "next/router";
+import React from "react";
+class TaskComponent extends React.Component {
+  render() {
+    console.log(this);
+    return (
+      <div>
+        <span>
+          {this.props.router && this.props.router.query.taskName} 任务负责人：
+          {this.props && this.props.fzr}
+        </span>
+      </div>
+    );
+  }
 }
-export default withRouter(Task)
+TaskComponent.getInitialProps = async () => {
+  console.log("----------服务器端渲染-------------");
+  await new Promise(resolve => {
+    setTimeout(resolve, 5000);
+  });
+  return { fzr: "chatwei" };
+};
+export default withRouter(TaskComponent);
